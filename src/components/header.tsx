df1b6bc5c859bd3c61626299c560012be67681cb
Lado Asambadze <lado.asambadze1@gmail.com>
2023-07-28 14:19:48 +0400

    feat: add styles for header, desktop resolution

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 import { styled } from "@mui/material";
 import Box from "@mui/material/Box/Box";
 import Typography from "@mui/material/Typography/Typography";
+import Button from "@mui/material/Button/Button";
 
 export default function Header() {
   return (
@@ -8,11 +9,19 @@ export default function Header() {
       <Main>
         <Description>
           <Logo src="/vite.svg" alt="Website pick up store logo" />
-          <Name>PickUp</Name>
+          <Name>Pick Up </Name>
+          <DestkopMenu>
+            <Type variant="outlined">Men</Type>
+            <Type variant="outlined">Women</Type>
+            <Type variant="outlined">Kids</Type>
+            <Type variant="outlined">Sale</Type>
+          </DestkopMenu>
         </Description>
+
         <Shop>
           <SearchDiv>
             <SearchIcon src="/search-black.png" alt="Search loop icon" />
+            <SearchInput placeholder="Search" />
           </SearchDiv>
 
           <Favourite
@@ -35,14 +44,23 @@ const Main = styled(Box)`
   justify-content: space-between;
   align-items: center;
   padding: 20px 15px 20px 15px;
+  @media (min-width: 1440px) {
+    padding: 30px 25px 30px 45px;
+  }
 `;
 
 const Description = styled(Box)`
   display: flex;
   flex-direction: row;
+  align-items: center;
 `;
 
-const Logo = styled("img")({});
+const Logo = styled("img")`
+  @media (min-width: 1440px) {
+    width: 40px;
+    height: 40px;
+  }
+`;
 
 const Name = styled(Typography)`
   color: white;
@@ -50,6 +68,10 @@ const Name = styled(Typography)`
   font-weight: 400;
   font-size: 24px;
   margin-left: 10px;
+  @media (min-width: 1440px) {
+    font-size: 28px;
+    margin-left: 15px;
+  }
 `;
 
 const Shop = styled(Box)`
@@ -66,31 +88,90 @@ const SearchDiv = styled(Box)`
   flex-direction: row;
   align-items: center;
   border: 4px solid #61e399;
+  @media (min-width: 1440px) {
+    border: none;
+    border-radius: 15px;
+    padding: 6px 8px 6px 10px;
+    border: 3px solid gray;
+    &:hover {
+      border-color: green;
+    }
+  }
 `;
 
-const SearchIcon = styled("img")({
-  width: "20px",
-  height: "20px",
-  cursor: "pointer",
-});
+const SearchInput = styled("input")`
+  display: none;
+  @media (min-width: 1440px) {
+    display: block;
+    width: 150px;
+    outline: none;
+    margin-left: 5px;
+    border: none;
+  }
+`;
 
-const Favourite = styled("img")({
-  width: `20px`,
-  height: `22px`,
-  marginLeft: `10px`,
-  cursor: `pointer`,
-});
+const SearchIcon = styled("img")`
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
 
-const Cart = styled("img")({
-  width: `25px`,
-  height: `25px`,
-  marginLeft: `10px`,
-  cursor: `pointer`,
-});
+  @media (min-width: 1440px) {
+    width: 23px;
+    height: 23px;
+  }
+`;
 
-const Menu = styled("img")({
-  width: `25px`,
-  height: `25px`,
-  marginLeft: `10px`,
-  cursor: `pointer`,
-});
+const Favourite = styled("img")`
+  width: 20px;
+  height: 22px;
+  margin-left: 15px;
+  cursor: pointer;
+  @media (min-width: 1440px) {
+    width: 30px;
+    height: 33px;
+    margin-left: 20px;
+  }
+`;
+
+const Cart = styled("img")`
+  width: 25px;
+  height: 25px;
+  margin-left: 15px;
+  cursor: pointer;
+  @media (min-width: 1440px) {
+    width: 30px;
+    height: 30px;
+    margin-left: 20px;
+  }
+`;
+
+const Menu = styled("img")`
+  width: 25px;
+  height: 25px;
+  margin-left: 15px;
+  cursor: pointer;
+  @media (min-width: 1440px) {
+    display: none;
+  }
+`;
+
+const DestkopMenu = styled(Box)`
+  display: none;
+  @media (min-width: 1440px) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-left: 40px;
+  }
+`;
+
+const Type = styled(Button)`
+  color: white;
+  margin-left: 15px;
+  margin-right: 15px;
+  border-color: gray;
+  padding: 2px 15px 2px 15px;
+  &:hover {
+    border-color: green;
+  }
+`;
