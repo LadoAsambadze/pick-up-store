c07b41cdd84c45b4d890153fcc2fb05d29d362c1
Lado Asambadze <lado.asambadze1@gmail.com>
2023-07-30 23:34:34 +0400

    change: change component row

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Header from "./components/header";
 import { styled } from "@mui/material";
 import Box from "@mui/material/Box/Box";
-
 import BrandCarousel from "./components/brandCarousel";
 import NewArrivals from "./components/newArrivals";
 
@@ -10,8 +9,8 @@ function App() {
     <>
       <Header />
       <Main>
-        <BrandCarousel />
         <NewArrivals />
+        <BrandCarousel />
       </Main>
     </>
   );
