a951a610170fa10607d981fa1657ed9a1d5955a2
Lado Asambadze <lado.asambadze1@gmail.com>
2023-08-17 00:11:36 +0400

    style: upgrade style of button in selected items

--- src/pages/selected.tsx
+++ src/pages/selected.tsx
@@ -64,8 +64,8 @@ export default function Selected() {
         <DivideDivSecond>
           <Description>
             <Name>Nike Pegasus Turbo Next Nature</Name>
-            <Brand>Brand: Puma</Brand>
-            <Price>$ 40.33</Price>
+            <Brand>Brand: {shoesItem?.brand}</Brand>
+            <Price>Price: ${shoesItem?.price}</Price>
           </Description>
 
           <Carousel responsive={responsiveSort} infinite={true}>
@@ -75,6 +75,8 @@ export default function Selected() {
                   key={index}
                   style={{
                     backgroundImage: `url(http://localhost:3000${item})`,
+                    border:
+                      selectedImage === item ? "1px solid #cf9f58" : "none",
                   }}
                   onClick={() => {
                     setSelectedImage(item);
@@ -84,22 +86,15 @@ export default function Selected() {
           </Carousel>
           <SizeDiv>
             <SizeHeader>Select Size</SizeHeader>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                flexWrap: "wrap",
-                paddingRight: "0%",
-              }}
-            >
+            <ItemSizeSection>
               {result &&
                 Object.entries(result.size).map(([key, value], index) => (
                   <SizeChoose
                     style={{
                       backgroundColor:
-                        key === selectedSize ? "lightblue" : "transparent",
+                        key === selectedSize ? "#90dfe2" : "transparent",
                       pointerEvents: value === 0 ? "none" : "auto",
-                      filter: value === 0 ? "blur(0.5px)" : "none",
+                      filter: value === 0 ? "blur(0.7px)" : "none",
                       opacity: value === 0 ? "0.5" : "1",
                     }}
                     key={index}
@@ -108,7 +103,7 @@ export default function Selected() {
                     {key}
                   </SizeChoose>
                 ))}
-            </div>
+            </ItemSizeSection>
             <SizeHeader>Select Color</SizeHeader>
             <Carousel
               responsive={responsiveColor}
@@ -122,6 +117,10 @@ export default function Selected() {
                     key={index}
                     style={{
                       backgroundImage: `url(http://localhost:3000${item.urls[0]})`,
+                      border:
+                        selectedImage === item.urls[0]
+                          ? "2px solid #cf9f58"
+                          : "none",
                     }}
                     onClick={() => {
                       setSelectedImage(item.urls[0]);
@@ -132,6 +131,11 @@ export default function Selected() {
                   ></SmallImageDiv>
                 ))}
             </Carousel>
+            <AddQuantity>
+              <Minus src="/icon-minus.svg" />
+              <Quantity>0</Quantity>
+              <Plus src="/icon-plus.svg" />
+            </AddQuantity>
             <AddToCart onClick={addToCart}>
               <CartIcon src="/cart-icon.svg" alt="Cart Icon" />
               <ButtonText>Add To Cart</ButtonText>
@@ -181,21 +185,31 @@ const Main = styled(Box)`
 const Description = styled(Box)`
   display: flex;
   flex-direction: column;
-  padding: 0px 25px 12px 25px;
+  padding: 0px 20px 12px 20px;
 `;
 
 const Name = styled(Typography)`
+  font-family: "Kumbh Sans", sans-serif;
+  font-weight: 700;
+  color: black;
   font-size: 20px;
   text-align: left;
 `;
 const Brand = styled(Typography)`
+  font-family: "Kumbh Sans", sans-serif;
+  font-weight: 500;
+  color: black;
   font-size: 14px;
   text-align: left;
+  margin-top: 5px;
 `;
 const Price = styled(Typography)`
-  font-size: 16px;
+  font-family: "Kumbh Sans", sans-serif;
+  font-weight: 700;
+  color: black;
+  font-size: 18px;
   text-align: left;
-  margin-top: 15px;
+  margin-top: 10px;
 `;
 const ImageDiv = styled("img")`
   width: 100%;
@@ -206,12 +220,12 @@ const ImageDiv = styled("img")`
 
 const SmallImageDiv = styled(Box)`
   height: 100px;
-
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
   margin: 5px 2px 2px 2px;
   cursor: pointer;
+  border-radius: 3px;
   @media (min-width: 500px) {
     height: 150px;
   }
@@ -238,23 +252,71 @@ const SmallImageDivSort = styled(Box)`
 `;
 
 const SizeDiv = styled(Box)`
-  padding: 20px 25px 12px 25px;
-`;
-const SizeHeader = styled(Typography)`
-  font-size: 20px;
+  padding: 20px 20px 12px 20px;
 `;
 
+const ItemSizeSection = styled(Box)`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+const SizeHeader = styled(Typography)`
+  font-family: "Kumbh Sans", sans-serif;
+  font-weight: 700;
+  color: black;
+  font-size: 16px;
+`;
 const SizeChoose = styled(Box)`
   border-radius: 8px;
-  border: 1px solid #bebcbd;
-  opacity: 0.8;
-  width: 65px;
-  height: 35px;
+  border: 1px solid #5e5a5c;
+  width: 55px;
+  height: 40px;
   display: flex;
   align-items: center;
   justify-content: center;
-  margin: 10px;
+  margin: 5px;
   cursor: pointer;
+  font-family: "Kumbh Sans", sans-serif;
+  font-weight: 400;
+  color: #534c4c;
+  font-size: 14px;
+`;
+
+const AddQuantity = styled("div")`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 70px 10px 70px;
+  width: 100%;
+  border: none;
+  color: black;
+  border-radius: 10px;
+  background: var(--light-grayish-blue, #f7f8fd);
+  margin-top: 10px;
+
+  @media (min-width: 900px) {
+    width: 70%;
+    margin-top: 20px;
+  }
+`;
+
+const Minus = styled("img")`
+  width: 16px;
+  height: 16px;
+  cursor: pointer;
+`;
+const Plus = styled("img")`
+  width: 16px;
+  height: 16px;
+  cursor: pointer;
+`;
+
+const Quantity = styled(Typography)`
+  font-family: "Kumbh Sans", sans-serif;
+  font-weight: 500;
+  color: black;
+  font-size: 17px;
 `;
 
 const AddToCart = styled("div")`
@@ -290,7 +352,10 @@ const HeartIcon = styled("img")`
   height: 16px;
 `;
 const ButtonText = styled(Typography)`
-  font-size: 12px;
+  font-family: "Kumbh Sans", sans-serif;
+  font-weight: 400px;
+  font-size: 13px;
+  color: white;
   margin-left: 7px;
 `;
 const AddToFav = styled("div")`
