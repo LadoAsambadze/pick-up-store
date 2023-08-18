cdeed9b2ca66481c36ece884ed9f5e3d3d296e59
Lado Asambadze <lado.asambadze1@gmail.com>
2023-08-18 14:13:56 +0400

    style: mark up of cart page until tablet layout

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography, styled } from "@mui/material";
+import { Box, Button, Typography, styled } from "@mui/material";
 import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
@@ -10,10 +10,12 @@ interface Type {
   quantity: number;
   image: string;
   amount: number;
+  price: number;
 }
 
 export default function Cart() {
   const [selectedProducts, setSelectedProducts] = useState<Type[]>([]);
+
   useEffect(() => {
     const getCart = async () => {
       const response = await axios.get(
@@ -29,23 +31,33 @@ export default function Cart() {
       <Main>
         {selectedProducts.map((item, index) => (
           <ProductDiv key={index}>
-            <ImageDiv
-              style={{
-                backgroundImage: `url(https://pick-up-store-backend-production.up.railway.app${item.image})`,
-              }}
-            ></ImageDiv>
+            <ImageDiv>
+              <img
+                style={{ maxWidth: "100%" }}
+                src={`https://pick-up-store-backend-production.up.railway.app${item.image}`}
+              />
+            </ImageDiv>
             <DescriptionDiv>
               <Name>{item.name}</Name>
-              <Size>Size: {item.size}</Size>
-              <h1>{item.color}</h1>
-              <PiceXquantity>
-                <Price>75$</Price>
-                <Quantity>Quantity: {item.quantity}</Quantity>
-              </PiceXquantity>
-              <ControlDiv>
+              <DescriptionSecondary>
+                <Size>Size: {item.size}</Size>
+                <Quantity>Avaliable: {item.quantity}</Quantity>
+              </DescriptionSecondary>
+
+              <DescriptionSecondary>
+                <Price>${item.price}</Price>
+                <AddQuantity>
+                  <Minus src="/icon-minus.svg" />
+                  <Quantity>{item.amount}</Quantity>
+                  <Plus src="/icon-plus.svg" />
+                </AddQuantity>
+              </DescriptionSecondary>
+
+              <DescriptionSecondary>
                 <ControlIcon src="/heart.svg" />
                 <ControlIcon src="/delete.png" />
-              </ControlDiv>
+                <Change variant="contained">Change</Change>
+              </DescriptionSecondary>
             </DescriptionDiv>
           </ProductDiv>
         ))}
@@ -59,62 +71,116 @@ const Main = styled(Box)`
   min-height: 100vh;
   display: flex;
   flex-direction: column;
+  align-items: center;
   padding-left: 20px;
   padding-right: 20px;
+  padding-top: 20px;
+  background: #f8f8f8;
 `;
 
 const ProductDiv = styled(Box)`
   display: flex;
   flex-direction: row;
-  padding: 10px;
-  overflow: hidden;
+  border-radius: 20px;
+  background: var(--white, #fff);
+  box-shadow: 0px 20px 35px 0px rgba(0, 0, 0, 0.05);
+  padding: 20px;
+  width: 100%;
+  margin-bottom: 15px;
 `;
 
 const ImageDiv = styled(Box)`
   width: 50%;
-  height: 150px;
+  height: 100%;
+  border-radius: 15px;
+  background: var(--secondary, #eef1f4);
   background-position: center;
   background-size: cover;
-  background-repeat: no-repeat;
+  overflow: hidden;
 `;
 
 const DescriptionDiv = styled(Box)`
   display: flex;
   flex-direction: column;
-  padding: 0px 0px 0px 20px;
-  width: 50%;
-  justify-content: space-between;
+  width: 100%;
+  margin-left: 25px;
 `;
 
 const Name = styled(Typography)`
+  font-family: "Kumbh Sans", sans-serif;
+  font-weight: 500;
   font-size: 16px;
-`;
-const Size = styled(Typography)`
-  font-size: 14px;
+  font-style: normal;
+  line-height: 18px;
 `;
 
-const PiceXquantity = styled(Box)`
+const Price = styled(Typography)`
+  font-size: 14px;
+  font-family: "Kumbh Sans", sans-serif;
+  font-weight: 400;
+  line-height: 15px;
+`;
+
+const Size = styled(Typography)`
+  font-size: 14px;
+  font-family: "Kumbh Sans", sans-serif;
+  font-weight: 400;
+  line-height: 15px;
+`;
+
+const DescriptionSecondary = styled(Box)`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  align-items: center;
+  margin-top: 25px;
+`;
+
+const ControlIcon = styled("img")`
+  width: 25px;
+  height: 25px;
+  cursor: pointer;
+  margin-right: 10px;
+`;
+
+const AddQuantity = styled("div")`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  width: 100%;
-`;
-const Price = styled(Typography)`
-  font-size: 16px;
+  background: var(--light-grayish-blue, #f4f5fd);
+  padding: 5px 10px 5px 10px;
+  border: none;
+  color: black;
+  border-radius: 10px;
+
+  margin-left: 25px;
 `;
 
-const Quantity = styled(Typography)`
-  font-size: 16px;
-`;
-
-const ControlDiv = styled(Box)`
-  display: flex;
-  flex-direction: row;
-`;
-
-const ControlIcon = styled("img")`
-  width: 30px;
-  height: 30px;
+const Minus = styled("img")`
+  width: 16px;
+  height: 16px;
   cursor: pointer;
+`;
+const Plus = styled("img")`
+  width: 16px;
+  height: 16px;
+  cursor: pointer;
+`;
+const Quantity = styled(Box)`
+  font-family: "Kumbh Sans", sans-serif;
+  font-weight: 500;
+  color: #08213d;
+  font-size: 14px;
+  margin-left: 20px;
+  margin-right: 20px;
+`;
+
+const Change = styled(Button)`
+  font-size: 10px;
+  font-family: "Kumbh Sans", sans-serif;
+  font-weight: 500;
+  background: #ff7d1a;
+  padding: 5px;
+  margin-left: 10px;
 `;
