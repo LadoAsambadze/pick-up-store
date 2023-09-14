366017b809705238eaa71e69877b39b1bf265457
Lado Asambadze <lado.asambadze1@gmail.com>
2023-09-14 11:42:11 +0400

    Streak: commit for my streak on github :D :D

--- src/pages/MyAdmin.tsx
+++ src/pages/MyAdmin.tsx
@@ -112,7 +112,7 @@ export default function Admin() {
                                 onClick={() => {
                                   sentOrder();
                                   setUserId(user);
-                                  setChoosedItems();
+                                  
                                 }}
                               >
                                 Move To Sent
